434f32866ad0a7099b4d26e1a971c52840e3a7b4
dan.shan <i@shanhh.com>
2014-04-27 17:31:07 +0800

    feature add test case for spring framework

--- demo/src/main/java/com/shanhh/testcase/demo/spring/SpringFramework.java
+++ demo/src/main/java/com/shanhh/testcase/demo/spring/SpringFramework.java
@@ -1,8 +1,16 @@
 package com.shanhh.testcase.demo.spring;
 
 /**
+ * 基于 spring framework 的方法
  * @author dan.shan
  * @since 2014-04-27 下午5:12
  */
-public class SpringFramework {
+public interface SpringFramework {
+
+    /**
+     * 基于 spring 的方法
+     * @return
+     */
+    String springFunction();
+
 }
